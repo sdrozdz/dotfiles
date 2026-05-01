4396be458cb39b5a4326671578795548cd12143e
Sebastian Drożdż <seba987xdc@gmail.com>
2026-05-01 22:37:54 +0200

    wezterm hide tabs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,21 +4,20 @@ local act = wezterm.action
 local is_windows = wezterm.target_triple:find("windows") ~= nil
 local is_mac = wezterm.target_triple:find("darwin")
 
-local action_key = 'ALT'
-local rev_action_key = 'ALT|SHIFT'
-
+local action_key = "ALT"
+local rev_action_key = "ALT|SHIFT"
 
 if is_windows then
-  config.default_domain = 'WSL:Ubuntu'
+	config.default_domain = "WSL:Ubuntu"
 end
 
-if is_mac then 
-  action_key = 'CMD'
-  rev_action_key = 'CMD|SHIFT'
+if is_mac then
+	action_key = "CMD"
+	rev_action_key = "CMD|SHIFT"
 end
 
 -- Colors
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 config.term = "xterm-256color"
 
 -- Font settings
@@ -28,55 +27,69 @@ config.font = wezterm.font("MesloLGL Nerd Font")
 -- Appearance
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 1
+config.hide_tab_bar_if_only_one_tab = true
 
 -- Keybidings
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = {
-  {
-    key = 's',
-    mods = 'LEADER',
-    action = wezterm.action.PaneSelect { mode = 'SwapWithActive' },
-  },
-  {
-    key = 'w',
-    mods = 'LEADER',
-    action = act.CloseCurrentPane { confirm = true },
-  }
+	{
+		key = "s",
+		mods = "LEADER",
+		action = wezterm.action.PaneSelect({ mode = "SwapWithActive" }),
+	},
+	{
+		key = "w",
+		mods = "LEADER",
+		action = act.CloseCurrentPane({ confirm = true }),
+	},
 }
 
 -- ALT: primary actions
 for _, v in ipairs({
-  { "Enter",      act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { "w",          act.CloseCurrentPane { confirm = true } },
-  { "t",          act.SpawnTab 'CurrentPaneDomain' },
-  { "h",          act.ActivatePaneDirection 'Left' },
-  { "l",          act.ActivatePaneDirection 'Right' },
-  { "j",          act.ActivatePaneDirection 'Up' },
-  { "k",          act.ActivatePaneDirection 'Down' },
-  { "LeftArrow",  act.ActivateTabRelative(-1) },
-  { "RightArrow", act.ActivateTabRelative(1) },
-}) do table.insert(config.keys, { mods = action_key, key = v[1], action = v[2] }) end
+	{ "Enter", act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ "w", act.CloseCurrentPane({ confirm = true }) },
+	{ "t", act.SpawnTab("CurrentPaneDomain") },
+	{ "h", act.ActivatePaneDirection("Left") },
+	{ "l", act.ActivatePaneDirection("Right") },
+	{ "j", act.ActivatePaneDirection("Up") },
+	{ "k", act.ActivatePaneDirection("Down") },
+	{ "LeftArrow", act.ActivateTabRelative(-1) },
+	{ "RightArrow", act.ActivateTabRelative(1) },
+}) do
+	table.insert(config.keys, { mods = action_key, key = v[1], action = v[2] })
+end
 
 -- ALT+SHIFT: additional actions
 for _, v in ipairs({
-  { "Enter", act.SplitVertical { domain = 'CurrentPaneDomain' } },
-  { "z",     act.TogglePaneZoomState },
-}) do table.insert(config.keys, { mods = rev_action_key, key = v[1], action = v[2] }) end
+	{ "Enter", act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ "z", act.TogglePaneZoomState },
+}) do
+	table.insert(config.keys, { mods = rev_action_key, key = v[1], action = v[2] })
+end
 
 -- ALT+1-8: goto tab
-for i = 0, 7 do table.insert(config.keys, { mods = "ALT", key = tostring(i + 1), action = act.ActivateTab(i) }) end
-
+for i = 0, 7 do
+	table.insert(config.keys, { mods = "ALT", key = tostring(i + 1), action = act.ActivateTab(i) })
+end
 
 -- Mouse bindings
 config.mouse_bindings = {
-  { event = { Down = { streak = 1, button = "Right" } }, mods = "NONE", action = act.CopyTo("Clipboard") },
-  { event = { Down = { streak = 1, button = "Middle" } }, mods = "NONE", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-  { event = { Down = { streak = 1, button = "Middle" } }, mods = "SHIFT", action = act.CloseCurrentPane { confirm = false } },
-  {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'CTRL',
-    action = wezterm.action.OpenLinkAtMouseCursor,
-  },
+	{ event = { Down = { streak = 1, button = "Right" } }, mods = "NONE", action = act.CopyTo("Clipboard") },
+	{
+		event = { Down = { streak = 1, button = "Middle" } },
+		mods = "NONE",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		event = { Down = { streak = 1, button = "Middle" } },
+		mods = "SHIFT",
+		action = act.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
 }
 
 -- Performance settings
@@ -85,21 +98,21 @@ config.max_fps = 120
 config.prefer_egl = true
 
 -- Hooks
-wezterm.on('update-status', function(window, pane)
-  local name = window:active_key_table()
-  local leader = ""
+wezterm.on("update-status", function(window, pane)
+	local name = window:active_key_table()
+	local leader = ""
 
-  -- Sprawdzenie czy Leader jest aktywny
-  if window:leader_is_active() then
-    leader = '  LEADER  '
-  end
+	-- Sprawdzenie czy Leader jest aktywny
+	if window:leader_is_active() then
+		leader = "  LEADER  "
+	end
 
-  window:set_right_status(wezterm.format({
-    { Foreground = { Color = '#ffffff' } },
-    { Background = { Color = '#ff0000' } }, 
-    { Attribute = { Intensity = 'Bold' } },
-    { Text = leader },
-  }))
+	window:set_right_status(wezterm.format({
+		{ Foreground = { Color = "#ffffff" } },
+		{ Background = { Color = "#ff0000" } },
+		{ Attribute = { Intensity = "Bold" } },
+		{ Text = leader },
+	}))
 end)
 
 return config
